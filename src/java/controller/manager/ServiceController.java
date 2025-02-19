685467bf97869bd13869fd6d6a65de1d6f66d2c0
LongNgoTapCode <longvstroll@gmail.com>
2025-02-20 01:42:21 +0700

    Update: Service List, Service Detail, Add Service

--- src/java/controller/manager/ServiceController.java
+++ src/java/controller/manager/ServiceController.java
@@ -20,7 +20,7 @@ import model.Service;
  *
  * @author CMD
  */
-@WebServlet(name = "ServiceListController", urlPatterns = {"/manager/ServicesList", "/manager/ServicesList/Add", "/manager/ServicesList/Update", "/manager/ServicesList/Delete"})
+@WebServlet(name = "ServiceListController", urlPatterns = {"/manager/ServiceList", "/manager/ServiceList/Add", "/manager/ServiceList/Update", "/manager/ServiceList/Delete", "/manager/ServiceDetail"})
 public class ServiceController extends HttpServlet {
 
     private ServiceDAO serviceDAO;
@@ -35,16 +35,19 @@ public class ServiceController extends HttpServlet {
             throws ServletException, IOException {
         String url = request.getServletPath();
         switch (url) {
-            case "/manager/ServicesList":
+            case "/manager/ServiceList":
                 handleServiceList(request, response);
                 break;
-            case "/manager/ServicesList/Add":
+            case "/manager/ServiceList/Add":
+                handleAddService(request, response);
+            case "/manager/ServiceList/Update":
                 break;
-            case "/manager/ServicesList/Update":
+            case "/manager/ServiceList/Delete":
                 break;
-            case "/manager/ServicesList/Delete":
-                break;
+            case "/manager/ServiceDetail":
+                handleServiceDetail(request, response);
         }
+
     }
 
     @Override
@@ -96,6 +99,81 @@ public class ServiceController extends HttpServlet {
         dispatcher.forward(request, response);
     }
 
+    private void handleServiceDetail(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // get parameter from request
+        String nameOrId = request.getParameter("nameOrId");
+        String statusParam = request.getParameter("status");
+        String pageParam = request.getParameter("page");
+        String sortBy = request.getParameter("sortBy");
+        String sortDir = request.getParameter("sortDir");
+
+        int status = (statusParam != null) ? Integer.parseInt(statusParam) : -1;
+        int page = (pageParam != null) ? Integer.parseInt(pageParam) : 1;
+        int limit = 3; // Số bản ghi mỗi trang
+        int offset = (page - 1) * limit;
+
+        if (sortBy == null || sortBy.isEmpty()) {
+            sortBy = "title";
+        }
+        if (sortDir == null || sortDir.isEmpty()) {
+            sortDir = "asc";
+        }
+
+        List<Service> services = serviceDAO.getAllService(offset, limit, nameOrId, -1, status, sortBy, sortDir);
+
+        int totalRecords = serviceDAO.countAllServices(nameOrId, -1, status);
+        int totalPages = (int) Math.ceil((double) totalRecords / limit);
+
+        request.setAttribute("services", services);
+        request.setAttribute("currentPage", page);
+        request.setAttribute("totalPages", totalPages);
+        request.setAttribute("nameOrId", nameOrId);
+        request.setAttribute("status", status);
+        request.setAttribute("sortBy", sortBy);
+        request.setAttribute("sortDir", sortDir);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/landing/manager/ServiceDetail.jsp");
+        dispatcher.forward(request, response);
+    }
+    
+    private void handleAddService(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // get parameter from request
+        String nameOrId = request.getParameter("nameOrId");
+        String statusParam = request.getParameter("status");
+        String pageParam = request.getParameter("page");
+        String sortBy = request.getParameter("sortBy");
+        String sortDir = request.getParameter("sortDir");
+
+        int status = (statusParam != null) ? Integer.parseInt(statusParam) : -1;
+        int page = (pageParam != null) ? Integer.parseInt(pageParam) : 1;
+        int limit = 3; // Số bản ghi mỗi trang
+        int offset = (page - 1) * limit;
+
+        if (sortBy == null || sortBy.isEmpty()) {
+            sortBy = "title";
+        }
+        if (sortDir == null || sortDir.isEmpty()) {
+            sortDir = "asc";
+        }
+
+        List<Service> services = serviceDAO.getAllService(offset, limit, nameOrId, -1, status, sortBy, sortDir);
+
+        int totalRecords = serviceDAO.countAllServices(nameOrId, -1, status);
+        int totalPages = (int) Math.ceil((double) totalRecords / limit);
+
+        request.setAttribute("services", services);
+        request.setAttribute("currentPage", page);
+        request.setAttribute("totalPages", totalPages);
+        request.setAttribute("nameOrId", nameOrId);
+        request.setAttribute("status", status);
+        request.setAttribute("sortBy", sortBy);
+        request.setAttribute("sortDir", sortDir);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/landing/manager/AddService.jsp");
+        dispatcher.forward(request, response);
+    }
     private void updateService(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
